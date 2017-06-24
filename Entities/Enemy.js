fc58d6c3044129e9b3295eaa51cf86d230c3bb9d
Rasmus <rasmusrendal@gmail.com>
2017-06-24 21:54:28 +0200

    Added enemy

---
+++ Entities/Enemy.js
@@ -0,0 +1,17 @@
+class Enemy extends Entity {
+	constructor(x, y, player) {
+		this.x = x;
+		this.y = y;
+		this.player = player;
+	}
+
+	turn() {
+		var xOffset = player.x - this.x;
+		var yOffset = player.y - this.y;
+		if (abs(xOffset) > abs(yOffset)) {
+			this.move(xOffset/abs(xOffset), 0);
+		} else {
+			this.move(0, yOffset/abs(yOffset));
+		}
+	}
+}
