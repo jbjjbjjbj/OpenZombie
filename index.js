e57fed64b73bb6e9a4806a42d2bcc2ceae6fbf82
benjadahl <benjadahl@gmail.com>
2017-06-24 20:47:03 +0200

    Added grid variable

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ console.log("Welcome to OpenZombie");
 
 const gridSize = {x: 10, y: 10};
 var gameTable = document.getElementById("gameTable");
+var grid = [];
 
 function initGrid (x, y) {
   var row;
   for (var i = 0; i < y; i++) {
     row = gameTable.insertRow(0);
+    grid[i] = [];
     for (var j = 0; j < x; j++) {
-      row.insertCell(-1);
+      grid[i][j] = row.insertCell(-1);
     }
   }
 }
 
 initGrid(gridSize.x, gridSize.y);
+
+console.log(grid);
