9c0331a4fa263911da1528e7f41cd733e4afde3b
benjadahl <benjadahl@gmail.com>
2017-06-24 20:42:32 +0200

    Made the grid generate

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Welcome to OpenZombie");
+
+const gridSize = {x: 10, y: 10};
+var gameTable = document.getElementById("gameTable");
+
+function initGrid (x, y) {
+  var row;
+  for (var i = 0; i < y; i++) {
+    row = gameTable.insertRow(0);
+    for (var j = 0; j < x; j++) {
+      row.insertCell(-1);
+    }
+  }
+}
+
+initGrid(gridSize.x, gridSize.y);
